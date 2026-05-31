3f28126c1462654080a3f41cc22eee5505b26dd3
Adeoye Favour <olamilekanfavour03@gmail.com>
2026-05-31 01:45:14 +0100

    chore: build project for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // or '@vitejs/plugin-react' depending on your setup
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/apex-estates-project/', // 👈 ADD THIS EXACT LINE WITH YOUR REPO NAME
+  base: '/apex-estates-project/',
 })
